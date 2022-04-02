480947d2bef268f8674cc50ad73265a909390867
Henrique Platero <contato.henriqueplatero@gmail.com>
2022-04-01 23:00:06 -0300

    deploy-final

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + 'dist/angulartopzera'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + 'dist/angulartopzera/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(PORT, () => {
