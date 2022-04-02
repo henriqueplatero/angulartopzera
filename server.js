e6207844cb43fbd42f0a7dd0bdb9a3d3a8180a19
Henrique Platero <contato.henriqueplatero@gmail.com>
2022-04-01 22:46:28 -0300

    heroku-deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + 'dist/angular-dtopzh'));
+app.use(express.static(__dirname + 'dist/angulartopzera'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + 'dist/angular-dtopzh/index.html');
+    res.sendFile(__dirname + 'dist/angulartopzera/index.html');
 });
 
 app.listen(PORT, () => {
